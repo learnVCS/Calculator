33dd98934de4c1db0980fac3ee268cc33e7fb97d
Aaron Sky <aaronsky@skyaaron.com>
2015-10-21 20:39:08 -0400

    Initial commit, adds two numbers passed as CLI args

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Convert.ToInt32(args[0]) + Convert.ToInt32(args[1]));
+        }
+    }
+}
